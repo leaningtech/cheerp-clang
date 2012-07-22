6ecabbffe8d0b4fc4f3b23bbf6a206970f7cc589
Alessandro Pignotti <a.pignotti@sssup.it>
2012-07-22 19:51:44 +0200

    Support serialization of return value

--- lib/Sema/SemaDeclAttr.cpp
+++ lib/Sema/SemaDeclAttr.cpp
@@ -4331,7 +4331,7 @@ static void handleServer(Sema &S, Decl *D, const AttributeList &Attr)
   //Stub for the client
   FunctionTemplateDecl* stubTemplateDecl=getTemplateFromName(S,"clientStub");
   Deduced.clear();
-  Deduced.push_back(DeducedTemplateArgument(TemplateArgument(F->getCallResultType())));
+  Deduced.push_back(DeducedTemplateArgument(TemplateArgument(resultType)));
   //Add the types of the function argument
   if(F->param_size()!=0)
       Deduced.push_back(DeducedTemplateArgument(TemplateArgument(&FArgsPack[0],FArgsPack.size())));
