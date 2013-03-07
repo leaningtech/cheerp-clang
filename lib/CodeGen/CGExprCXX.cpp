963b26005d059bbc3a71dac913a0a4fa5857772d
Alessandro Pignotti <a.pignotti@sssup.it>
2013-03-07 16:26:34 +0100

    What's the point of null checking placement new?

--- lib/CodeGen/CGExprCXX.cpp
+++ lib/CodeGen/CGExprCXX.cpp
@@ -1192,6 +1192,7 @@ llvm::Value *CodeGenFunction::EmitCXXNewExpr(const CXXNewExpr *E) {
   // CXXNewExpr::shouldNullCheckAllocation()) and we have an
   // interesting initializer.
   bool nullCheck = allocatorType->isNothrow(getContext()) &&
+    !allocator->isReservedGlobalPlacementOperator() &&
     (!allocType.isPODType(getContext()) || E->hasInitializer());
 
   llvm::BasicBlock *nullCheckBB = 0;
