05fd81c9ff57f6dc0a36ff5b7f59291bbdf7b5c2
Alessandro Pignotti <a.pignotti@sssup.it>
2013-07-03 22:30:56 +0200

    Identify the duetto target as little endian

--- lib/Basic/Targets/WebAssembly.h
+++ lib/Basic/Targets/WebAssembly.h
@@ -181,6 +181,8 @@ public:
 
     if (Opts.CPlusPlus)
       Builder.defineMacro("_GNU_SOURCE");
+
+    Builder.defineMacro("__LITTLE_ENDIAN__");
   }
 
   virtual BuiltinVaListKind getBuiltinVaListKind() const {
