5e0f477461624238de10be408bbf83e8583c6b19
Alessandro Pignotti <a.pignotti@sssup.it>
2013-08-25 13:23:31 +0200

    Fixes depending on the now conditional conversions between GEP,0,0->bitcast
    and the other way around

--- lib/CodeGen/CGCall.cpp
+++ lib/CodeGen/CGCall.cpp
@@ -2682,7 +2682,7 @@ static llvm::Value *tryRemoveRetainOfSelf(CodeGenFunction &CGF,
 
   // Look for a retain call.
   llvm::CallInst *retainCall =
-    dyn_cast<llvm::CallInst>(result->stripPointerCastsSafe());
+    dyn_cast<llvm::CallInst>(result->stripPointerCasts(CGF.getTarget().isByteAddressable()));
   if (!retainCall ||
       retainCall->getCalledValue() != CGF.CGM.getObjCEntrypoints().objc_retain)
     return nullptr;
