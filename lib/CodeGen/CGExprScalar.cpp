706063df8ba4229d3115ddd5cf653dda86fd5a36
Alessandro Pignotti <a.pignotti@sssup.it>
2013-09-09 21:59:45 +0200

    Do not use duetto.cast.user intrinsic on NULL values

--- lib/CodeGen/CGExprScalar.cpp
+++ lib/CodeGen/CGExprScalar.cpp
@@ -2026,7 +2026,7 @@ Value *ScalarExprEmitter::VisitCastExpr(CastExpr *CE) {
           CGF.getDebugInfo()->
               addHeapAllocSiteMetadata(CI, CE->getType(), CE->getExprLoc());
 
-    if (!CGF.getTarget().isByteAddressable())
+    if (!CGF.getTarget().isByteAddressable() && !isa<llvm::ConstantPointerNull>(Src))
     {
       // Add an intrincic to tag the cast as one requested by the user
       // And also emit a warning
