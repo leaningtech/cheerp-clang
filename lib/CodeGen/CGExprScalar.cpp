ba630dfd10416bde702b352432d2832c874755f0
Alessandro Pignotti <a.pignotti@sssup.it>
2013-09-09 17:20:42 +0200

    Duetto: Use the cast.user intrinsics to flag casts that must be allowed by the backend
    And use a warning to alert the user of the potential issues

--- lib/CodeGen/CGExprScalar.cpp
+++ lib/CodeGen/CGExprScalar.cpp
@@ -29,6 +29,7 @@
 #include "llvm/IR/GlobalVariable.h"
 #include "llvm/IR/Intrinsics.h"
 #include "llvm/IR/Module.h"
+#include "clang/Sema/SemaDiagnostic.h"
 #include <cstdarg>
 
 using namespace clang;
@@ -1302,6 +1303,16 @@ Value *ScalarExprEmitter::VisitCastExpr(CastExpr *CE) {
         SrcTy->getPointerAddressSpace() != DstTy->getPointerAddressSpace()) {
       llvm::Type *MidTy = CGF.CGM.getDataLayout().getIntPtrType(SrcTy);
       return Builder.CreateIntToPtr(Builder.CreatePtrToInt(Src, MidTy), DstTy);
+    }
+    if (!CGF.getTarget().isByteAddressable())
+    {
+      // Add an intrincic to tag the cast as one requested by the user
+      // And also emit a warning
+      CGF.CGM.getDiags().Report(CE->getLocStart(), diag::warn_duetto_unsafe_cast);
+
+      llvm::Value* tmp1=Builder.CreateBitCast(Src, CGF.Int8PtrTy);
+      llvm::Function* intrinsic = llvm::Intrinsic::getDeclaration(&CGF.CGM.getModule(), llvm::Intrinsic::duetto_cast_user);
+      Src = Builder.CreateCall(intrinsic, tmp1);
     }
     return Builder.CreateBitCast(Src, DstTy);
   }
