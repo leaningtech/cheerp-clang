ff19cd7a5f74f2af9c81eb2e569b7abdb6dc9136
Alessandro Pignotti <a.pignotti@sssup.it>
2013-10-13 19:26:10 +0200

    Use typed user cast on NBA targets

--- lib/CodeGen/CGExprScalar.cpp
+++ lib/CodeGen/CGExprScalar.cpp
@@ -1356,15 +1356,23 @@ Value *ScalarExprEmitter::VisitCastExpr(CastExpr *CE) {
       llvm_unreachable("wrong cast for pointers in different address spaces"
                        "(must be an address space cast)!");
     }
-    if (!CGF.getTarget().isByteAddressable() && !isa<llvm::ConstantPointerNull>(Src))
+    //We don't care about casts to functions types
+    if (CGF.getTarget().isByteAddressable() || isa<llvm::ConstantPointerNull>(Src) ||
+        (isa<llvm::Function>(Src) && isa<llvm::FunctionType>(DstTy)))
+    {
+      return Builder.CreateBitCast(Src, DstTy);
+    }
+    else
     {
       // Add an intrincic to tag the cast as one requested by the user
       // And also emit a warning
       CGF.CGM.getDiags().Report(CE->getLocStart(), diag::warn_duetto_unsafe_cast);
 
-      llvm::Value* tmp1=Builder.CreateBitCast(Src, CGF.Int8PtrTy);
-      llvm::Function* intrinsic = llvm::Intrinsic::getDeclaration(&CGF.CGM.getModule(), llvm::Intrinsic::duetto_cast_user);
-      Src = Builder.CreateCall(intrinsic, tmp1);
+      llvm::Type* types[] = { DstTy, SrcTy };
+
+      llvm::Function* intrinsic = llvm::Intrinsic::getDeclaration(&CGF.CGM.getModule(),
+                                        llvm::Intrinsic::duetto_cast_user, types);
+      return Builder.CreateCall(intrinsic, Src);
     }
     return Builder.CreateBitCast(Src, DstTy);
   }
