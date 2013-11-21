5ca15232acf9fe9a74e3ae5226448fbf7d204a10
Alessandro Pignotti <a.pignotti@sssup.it>
2013-11-21 03:10:04 +0100

    Generate the right tools in the duetto toolchain

--- lib/Driver/ToolChains.h
+++ lib/Driver/ToolChains.h
@@ -640,6 +640,8 @@ protected:
 };
 
 class LLVM_LIBRARY_VISIBILITY Duetto : public ToolChain {
+private:
+  mutable OwningPtr<tools::duetto::DuettoCompiler> DuettoCompiler;
 public:
   Duetto(const Driver &D, const llvm::Triple& Triple,
          const llvm::opt::ArgList &Args);
@@ -655,6 +657,9 @@ public:
   virtual void
   AddClangCXXStdlibIncludeArgs(const llvm::opt::ArgList &DriverArgs,
                                llvm::opt::ArgStringList &CC1Args) const;
+
+  virtual Tool *buildLinker() const;
+  virtual Tool *getTool(Action::ActionClass AC) const;
 };
 
 } // end namespace toolchains
