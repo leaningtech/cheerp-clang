62ac1a3051f100d1d022b8e398e45f9dd67bb346
Alessandro Pignotti <a.pignotti@sssup.it>
2014-03-05 16:32:14 +0100

    Do not generate user casts intrinsics if the type do not change

--- lib/CodeGen/CGExpr.cpp
+++ lib/CodeGen/CGExpr.cpp
@@ -2836,7 +2836,7 @@ LValue CodeGenFunction::EmitCastLValue(const CastExpr *E) {
     LValue LV = EmitLValue(E->getSubExpr());
     llvm::Value *V = LV.getAddress();
     llvm::Type* DestType = ConvertType(CE->getTypeAsWritten());
-    if (CGM.getTarget().isByteAddressable())
+    if (CGM.getTarget().isByteAddressable() || DestType==V->getType())
     {
       V = Builder.CreateBitCast(V, DestType);
     }
