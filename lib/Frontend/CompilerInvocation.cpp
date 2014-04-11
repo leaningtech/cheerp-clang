e420cbf614b9c7b51bb6d495719054c350eacd33
Alessandro Pignotti <a.pignotti@sssup.it>
2014-04-11 13:48:16 +0200

    Enable GNU extensions by default

--- lib/Frontend/CompilerInvocation.cpp
+++ lib/Frontend/CompilerInvocation.cpp
@@ -1187,7 +1187,7 @@ void CompilerInvocation::setLangDefaults(LangOptions &Opts, InputKind IK,
     case IK_PreprocessedCXX:
     case IK_ObjCXX:
     case IK_PreprocessedObjCXX:
-      LangStd = LangStandard::lang_cxx11;
+      LangStd = LangStandard::lang_gnucxx11;
       break;
     }
   }
