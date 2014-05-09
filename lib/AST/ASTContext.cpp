659173c941a7331b392392baef428b4eee456db1
Alessandro Pignotti <a.pignotti@sssup.it>
2014-05-09 13:52:17 +0200

    Improve robustness when compiling C code

--- lib/AST/ASTContext.cpp
+++ lib/AST/ASTContext.cpp
@@ -679,7 +679,7 @@ ASTContext::getCanonicalTemplateTemplateParmDecl(
 }
 
 CXXABI *ASTContext::createCXXABI(const TargetInfo &T) {
-  if (!LangOpts.CPlusPlus) return nullptr;
+  if (!LangOpts.CPlusPlus && T.isByteAddressable()) return nullptr;
 
   switch (T.getCXXABI().getKind()) {
   case TargetCXXABI::GenericARM: // Same as Itanium at this level
