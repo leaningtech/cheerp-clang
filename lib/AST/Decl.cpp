7ddec6141fcf93858db06a119ac5ada0028ea463
Alessandro Pignotti <a.pignotti@sssup.it>
2014-06-14 19:01:06 +0200

    Add support for fully typed builtins

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -3083,6 +3083,8 @@ unsigned FunctionDecl::getBuiltinID(bool ConsiderWrapperFunctions) const {
     return 0;
 
   ASTContext &Context = getASTContext();
+  if (Context.BuiltinInfo.isFullyTyped(BuiltinID))
+    return BuiltinID;
   if (Context.getLangOpts().CPlusPlus) {
     const auto *LinkageDecl =
         dyn_cast<LinkageSpecDecl>(getFirstDecl()->getDeclContext());
