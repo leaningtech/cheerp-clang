4bff3e20834aaa4511348c62d84761b6ba3918b0
Alessandro Pignotti <a.pignotti@sssup.it>
2014-08-03 13:28:02 +0200

    Remove special handling for memory intrinsics on BA

--- lib/CodeGen/CGBuiltin.cpp
+++ lib/CodeGen/CGBuiltin.cpp
@@ -1279,12 +1279,6 @@ RValue CodeGenFunction::EmitBuiltinExpr(const FunctionDecl *FD,
         QualType SrcType = SrcE->getType()->getPointeeType().getCanonicalType().getUnqualifiedType();
         if (DestType != SrcType)
           CGM.getDiags().Report(SrcE->getLocStart(), diag::err_cheerp_memintrinsic_same_type);
-        // Revert to the original arguments, unions are handled like on BA
-        if (DestType->isUnionType())
-        {
-          DestE = E->getArg(0);
-          SrcE = E->getArg(1);
-        }
       }
     }
     Address Dest = EmitPointerWithAlignment(DestE);
@@ -1363,12 +1357,6 @@ RValue CodeGenFunction::EmitBuiltinExpr(const FunctionDecl *FD,
         QualType SrcType = SrcE->getType()->getPointeeType().getCanonicalType().getUnqualifiedType();
         if (DestType != SrcType)
           CGM.getDiags().Report(SrcE->getLocStart(), diag::err_cheerp_memintrinsic_same_type);
-        // Revert to the original arguments, unions are handled like on BA
-        if (DestType->isUnionType())
-        {
-          DestE = E->getArg(0);
-          SrcE = E->getArg(1);
-        }
       }
     }
     Address Dest = EmitPointerWithAlignment(DestE);
@@ -1394,10 +1382,6 @@ RValue CodeGenFunction::EmitBuiltinExpr(const FunctionDecl *FD,
       {
         // Discard the cast to void*
         DestE = DestCast->getSubExpr();
-        QualType DestType = DestE->getType()->getPointeeType().getCanonicalType().getUnqualifiedType();
-        // Revert to the original arguments, unions are handled like on BA
-        if (DestType->isUnionType())
-          DestE = E->getArg(0);
       }
     }
     Address Dest = EmitPointerWithAlignment(DestE);
