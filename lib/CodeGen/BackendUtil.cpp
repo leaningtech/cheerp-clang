59d379fa8f141377e12295947e0adb196c207890
Alessandro Pignotti <a.pignotti@sssup.it>
2014-08-03 13:32:40 +0200

    On cheerp lower memory intrinsics to loop form using the right pass

--- lib/CodeGen/BackendUtil.cpp
+++ lib/CodeGen/BackendUtil.cpp
@@ -24,6 +24,7 @@
 #include "llvm/Bitcode/BitcodeWriter.h"
 #include "llvm/Bitcode/BitcodeWriterPass.h"
 #include "llvm/Cheerp/NativeRewriter.h"
+#include "llvm/Cheerp/StructMemFuncLowering.h"
 #include "llvm/CodeGen/RegAllocRegistry.h"
 #include "llvm/CodeGen/SchedulerRegistry.h"
 #include "llvm/CodeGen/TargetSubtargetInfo.h"
@@ -541,12 +542,18 @@ getInstrProfOptions(const CodeGenOptions &CodeGenOpts,
 }
 
 static void addCheerpPasses(const PassManagerBuilder &Builder,
-                                   PassManagerBase &PM) {
+                            PassManagerBase &PM) {
   //Run InstCombine first, to remove load/stores for the this argument
   PM.add(createInstructionCombiningPass());
   PM.add(createCheerpNativeRewriterPass());
   //Cheerp is single threaded, convert atomic instructions to regular ones
   PM.add(createLowerAtomicPass());
+}
+
+static void addPostInlineCheerpPasses(const PassManagerBuilder &Builder,
+                                      PassManagerBase &PM) {
+  //Memory intrinsic on structs should be decomposed
+  PM.add(createStructMemFuncLowering());
 }
 
 void EmitAssemblyHelper::CreatePasses(legacy::PassManager &MPM,
@@ -563,8 +570,14 @@ void EmitAssemblyHelper::CreatePasses(legacy::PassManager &MPM,
   Triple TargetTriple(TheModule->getTargetTriple());
 
   if (TargetTriple.getArch() == llvm::Triple::cheerp)
+  {
     PMBuilder.addExtension(PassManagerBuilder::EP_EarlyAsPossible,
                            addCheerpPasses);
+    PMBuilder.addExtension(PassManagerBuilder::EP_EnabledOnOptLevel0,
+                           addPostInlineCheerpPasses);
+    PMBuilder.addExtension(PassManagerBuilder::EP_LoopOptimizerEnd,
+                           addPostInlineCheerpPasses);
+  }
 
   std::unique_ptr<TargetLibraryInfoImpl> TLII(
       createTLII(TargetTriple, CodeGenOpts));
