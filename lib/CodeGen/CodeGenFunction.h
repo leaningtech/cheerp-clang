f8ef339c11664e151446fb77ef2bb1637c59d0b5
Alessandro Pignotti <a.pignotti@sssup.it>
2015-10-29 13:49:35 +0100

    Fix construction of multi-dimensional arrays of C++ objects

--- lib/CodeGen/CodeGenFunction.h
+++ lib/CodeGen/CodeGenFunction.h
@@ -2588,6 +2588,7 @@ public:
                                   bool ZeroInitialization = false);
 
   void EmitCXXAggrConstructorCall(const CXXConstructorDecl *D,
+                                  QualType elementType,
                                   llvm::Value *NumElements,
                                   Address ArrayPtr,
                                   const CXXConstructExpr *E,
