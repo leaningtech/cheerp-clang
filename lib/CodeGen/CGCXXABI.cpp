57858fd1edc5a97a5fa9233b3276f518d240cbe0
Alessandro Pignotti <a.pignotti@sssup.it>
2016-04-14 12:31:03 +0200

    Improve code quality of delete calls

--- lib/CodeGen/CGCXXABI.cpp
+++ lib/CodeGen/CGCXXABI.cpp
@@ -200,8 +200,6 @@ void CGCXXABI::ReadArrayCookie(CodeGenFunction &CGF, Address ptr,
                                const CXXDeleteExpr *expr, QualType eltTy,
                                llvm::Value *&numElements,
                                llvm::Value *&allocPtr, CharUnits &cookieSize) {
-  // Derive a char* in the same address space as the pointer.
-  ptr = CGF.Builder.CreateElementBitCast(ptr, CGF.Int8Ty);
 
   // If we don't need an array cookie, bail out early.
   if (!requiresArrayCookie(expr, eltTy)) {
@@ -211,6 +209,8 @@ void CGCXXABI::ReadArrayCookie(CodeGenFunction &CGF, Address ptr,
     return;
   }
 
+  // Derive a char* in the same address space as the pointer.
+  ptr = CGF.Builder.CreateElementBitCast(ptr, CGF.Int8Ty);
   cookieSize = getArrayCookieSizeImpl(eltTy);
   Address allocAddr =
     CGF.Builder.CreateConstInBoundsByteGEP(ptr, -cookieSize);
