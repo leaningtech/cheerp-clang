f2f86595738dd6561f494eefef8a03ebf214138b
Alessandro Pignotti <a.pignotti@sssup.it>
2016-04-19 15:32:53 +0200

    Fix dynamic_cast of reference types

--- lib/CodeGen/ItaniumCXXABI.cpp
+++ lib/CodeGen/ItaniumCXXABI.cpp
@@ -1385,7 +1385,6 @@ llvm::Value *ItaniumCXXABI::EmitDynamicCastCall(
     Result->addIncoming(ZeroDowncast, ZeroBB);
     Result->addIncoming(FailedDowncast, FailedBB);
     Value = Result;
-    CGF.Builder.CreateBr(CastEnd);
   } else
     Value = CGF.Builder.CreateBitCast(Value, DestLTy);
 
