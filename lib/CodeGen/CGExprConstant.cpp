89fb8936c737f13a648e1a2092ee307446182ce9
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-02-23 11:40:41 +0000

    Fix: skip GEPs only if they have zero offset

--- lib/CodeGen/CGExprConstant.cpp
+++ lib/CodeGen/CGExprConstant.cpp
@@ -1741,6 +1741,7 @@ private:
     llvm::Type* CurrentType = C->getType()->getPointerElementType();
     Indexes.push_back(llvm::ConstantInt::get(Int32Ty, 0));
     llvm::Type* DestTy = CGM.getTypes().ConvertTypeForMem(DestType);
+    bool allZeroGeps = true;
     while(DestTy->isPointerTy() && (OffsetVal || CurrentType!=DestTy->getPointerElementType()))
     {
       if (llvm::StructType* ST=dyn_cast<llvm::StructType>(CurrentType))
@@ -1764,6 +1765,8 @@ private:
         }
         const llvm::StructLayout *SL = CGM.getDataLayout().getStructLayout(ST);
         unsigned Index = SL->getElementContainingOffset(OffsetVal);
+        if (Index != 0)
+          allZeroGeps = false;
         Indexes.push_back(llvm::ConstantInt::get(CGM.Int32Ty, Index));
         OffsetVal -= SL->getElementOffset(Index);
         CurrentType = ST->getElementType(Index);
@@ -1773,6 +1776,8 @@ private:
         llvm::Type *ElementTy = AT->getElementType();
         unsigned ElementSize = CGM.getDataLayout().getTypeAllocSize(ElementTy);
         unsigned Index = OffsetVal / ElementSize;
+        if (Index != 0)
+          allZeroGeps = false;
         Indexes.push_back(llvm::ConstantInt::get(CGM.Int32Ty, Index));
         OffsetVal -= Index * ElementSize;
         CurrentType = ElementTy;
@@ -1781,7 +1786,7 @@ private:
         break;
     }
 
-    if (CurrentType == DestTy->getPointerElementType())
+    if (CurrentType == DestTy->getPointerElementType() || !allZeroGeps)
       C = llvm::ConstantExpr::getGetElementPtr(C, Indexes);
 
     if (OffsetVal == 0)
