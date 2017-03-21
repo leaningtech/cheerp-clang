77862d1a229a700bd7d85a4a6be725a1543878e4
Alessandro Pignotti <a.pignotti@sssup.it>
2017-03-21 14:43:18 +0100

    Fix linkage of thunks for pointers to member functions

--- lib/CodeGen/CodeGenModule.cpp
+++ lib/CodeGen/CodeGenModule.cpp
@@ -2768,6 +2768,7 @@ void CodeGenModule::EmitGlobalDefinition(GlobalDecl GD, llvm::GlobalValue *GV) {
          TI.Method = Method;
          TI.This.AdjustmentTarget = Method->getParent();
          getVTables().emitThunk(GlobalDecl(Method), TI, false);
+         GV->setLinkage(llvm::GlobalValue::LinkOnceODRLinkage);
       } else
         EmitGlobalFunctionDefinition(GD, GV);
 
