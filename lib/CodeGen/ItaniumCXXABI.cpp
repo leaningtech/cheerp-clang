5480911a738ea18f7a97e90cb91a5c4ee9a7fd56
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-04-13 09:45:52 +0000

    Add support for dynamic_cast in asm.js

--- lib/CodeGen/ItaniumCXXABI.cpp
+++ lib/CodeGen/ItaniumCXXABI.cpp
@@ -1377,15 +1377,14 @@ llvm::Value *ItaniumCXXABI::EmitTypeid(CodeGenFunction &CGF,
   llvm::Value* Value = NULL;
   if(CGF.getTarget().isByteAddressable()) {
     Value = CGF.GetVTablePtr(ThisPtr, StdTypeInfoPtrTy->getPointerTo(), ClassDecl);
+    Value = CGF.Builder.CreateConstInBoundsGEP1_64(Value, -1ULL);
   } else {
     llvm::Type* VTableType = CGM.getTypes().GetVTableType(ClassDecl)->getPointerTo();
     Value = CGF.GetVTablePtr(ThisPtr, VTableType);
+    bool asmjs = SrcRecordTy->getAsCXXRecordDecl()->hasAttr<AsmJSAttr>(); 
+    int offset = asmjs? 1 : 0;
+    Value = CGF.Builder.CreateStructGEP(VTableType->getPointerElementType(), Value, offset);
   }
-  // Load the type info.
-  if(!CGF.getTarget().isByteAddressable())
-    Value = CGF.Builder.CreateStructGEP(Value, 0);
-  else
-    Value = CGF.Builder.CreateConstInBoundsGEP1_64(Value, -1ULL);
   return CGF.Builder.CreateAlignedLoad(Value, CGF.getPointerAlign());
 }
 
@@ -1685,7 +1684,7 @@ void ItaniumCXXABI::emitVTableDefinitions(CodeGenVTables &CGVT,
   // Create and set the initializer.
   ConstantInitBuilder Builder(CGM);
   auto Components = Builder.beginStruct();
-  CGVT.createVTableInitializer(Components, VTLayout, RTTI);
+  CGVT.createVTableInitializer(Components, RD, VTLayout, RTTI);
   Components.finishAndSetAsInitializer(VTable);
 
   // Set the correct linkage.
@@ -1794,7 +1793,7 @@ llvm::GlobalVariable *ItaniumCXXABI::getAddrOfVTable(const CXXRecordDecl *RD,
 
   const VTableLayout &VTLayout =
       CGM.getItaniumVTableContext().getVTableLayout(RD);
-  llvm::Type *VTableType = CGM.getVTables().getVTableType(VTLayout);
+  llvm::Type *VTableType = CGM.getVTables().getVTableType(VTLayout, RD);
 
   // Use pointer alignment for the vtable. Otherwise we would align them based
   // on the size of the initializer which doesn't make sense as only single
@@ -3332,7 +3331,13 @@ void ItaniumRTTIBuilder::BuildVTablePointer(const Type *Ty) {
   }
 
   if(!CGM.getTarget().isByteAddressable()) {
-    llvm::Type* WrapperTypes[] = {CGM.getTypes().GetVTableType(8)};
+    bool asmjs = false;
+    if (Ty->isRecordType()){
+      asmjs = cast<CXXRecordDecl>(cast<RecordType>(Ty)->getDecl())->hasAttr<AsmJSAttr>();
+    } else {
+      asmjs = CGM.getLangOpts().getCheerpMode() == LangOptions::CHEERP_MODE_AsmJS;
+    }
+    llvm::Type* WrapperTypes[] = {CGM.getTypes().GetVTableType(8, asmjs)};
     llvm::Constant *VTable = CGM.getModule().getOrInsertGlobal(VTableName, llvm::StructType::get(CGM.getLLVMContext(), WrapperTypes, false, NULL));
     llvm::Constant *Zero = llvm::ConstantInt::get(CGM.Int32Ty, 0);
     llvm::SmallVector<llvm::Constant*, 2> GepIndexes;
@@ -3821,7 +3826,8 @@ void ItaniumRTTIBuilder::BuildVMIClassTypeInfo(const CXXRecordDecl *RD) {
         CGM.getItaniumVTableContext().getVirtualBaseOffsetOffset(RD, BaseDecl);
     else {
       const ASTRecordLayout &Layout = CGM.getContext().getASTRecordLayout(RD);
-      if(!CGM.getTarget().isByteAddressable() && Layout.getPrimaryBase() != BaseDecl && !BaseDecl->isEmpty()) {
+	bool asmjs = RD->hasAttr<AsmJSAttr>();
+      if(!asmjs && !CGM.getTarget().isByteAddressable() && Layout.getPrimaryBase() != BaseDecl && !BaseDecl->isEmpty()) {
         const CGRecordLayout &CGLayout = CGM.getTypes().getCGRecordLayout(RD);
         unsigned baseId = CGLayout.getNonVirtualBaseLLVMFieldNo(BaseDecl);
         Offset = CharUnits::fromQuantity(CGLayout.getTotalOffsetToBase(baseId));
