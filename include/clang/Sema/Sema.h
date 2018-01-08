ebcba896e69b7fbb1c5d9bffbf14155f55ae2013
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-01-08 16:26:49 +0100

    Check that a wasm function signature does not use genericjs types

--- include/clang/Sema/Sema.h
+++ include/clang/Sema/Sema.h
@@ -10527,6 +10527,15 @@ public:
       Expr *E,
       llvm::function_ref<void(Expr *, RecordDecl *, FieldDecl *, CharUnits)>
           Action);
+
+  // CHEERP: Utility function for checking if a type can be used in the asmjs section
+  static bool isAsmJSCompatible(QualType pt) {
+    pt = pt.getNonReferenceType();
+    while (pt->isAnyPointerType())
+      pt = pt->getPointeeType();
+    TagDecl* pd = pt->getAsTagDecl();
+    return !pd || pd->hasAttr<AsmJSAttr>();
+  }
 };
 
 /// \brief RAII object that enters a new expression evaluation context.
