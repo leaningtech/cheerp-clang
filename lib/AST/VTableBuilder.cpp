f978d6e1fe785c129409fd941494a5824222a5f0
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-03-15 17:18:21 +0100

    Add support for vbases in performTypeAdjustment (for This adjustments)

--- lib/AST/VTableBuilder.cpp
+++ lib/AST/VTableBuilder.cpp
@@ -1569,7 +1569,7 @@ ThisAdjustment ItaniumVTableBuilder::ComputeThisAdjustment(
         
       VCallOffsets = Builder.getVCallOffsets();
     }
-      
+    Adjustment.Virtual.Itanium.VirtualBase = Offset.VirtualBase;
     Adjustment.Virtual.Itanium.VCallOffsetOffset =
       VCallOffsets.getVCallOffsetOffset(MD).getQuantity();
   }
