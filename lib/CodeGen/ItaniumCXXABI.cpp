0795b9fc7f38ad77d63679ea813c8cd32d5f2fc2
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-03-20 13:28:57 +0100

    Fix performTypeAdjustment to exit immediately if no adjustment is needed

--- lib/CodeGen/ItaniumCXXABI.cpp
+++ lib/CodeGen/ItaniumCXXABI.cpp
@@ -1992,6 +1992,9 @@ static llvm::Value *performTypeAdjustment(CodeGenFunction &CGF,
                                           bool IsReturnAdjustment,
                                           const CXXRecordDecl* AdjustmentTarget,
                                           const CXXRecordDecl* AdjustmentSource) {
+  if (!NonVirtualAdjustment && !VirtualAdjustment)
+    return InitialPtr.getPointer();
+
   bool asmjs = AdjustmentTarget->hasAttr<AsmJSAttr>();
   // Cheerp: Handle non byte addressable case first
   if (!CGF.getTarget().isByteAddressable())
@@ -2030,9 +2033,6 @@ static llvm::Value *performTypeAdjustment(CodeGenFunction &CGF,
     }
     return Ptr;
   }
-
-  if (!NonVirtualAdjustment && !VirtualAdjustment)
-    return InitialPtr.getPointer();
 
   Address V = CGF.Builder.CreateElementBitCast(InitialPtr, CGF.Int8Ty);
 
