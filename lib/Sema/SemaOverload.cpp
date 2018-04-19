79460c04e83c7f17f4174cf4001f630597c44955
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-04-19 17:15:12 +0200

    Allow to directly pass an asmjs function as argument to an asmjs function from genericjs code

--- lib/Sema/SemaOverload.cpp
+++ lib/Sema/SemaOverload.cpp
@@ -12164,8 +12164,9 @@ static ExprResult FinishOverloadedCallExpr(Sema &SemaRef, Scope *S, Expr *Fn,
     Fn = SemaRef.FixOverloadedFunctionReference(Fn, (*Best)->FoundDecl, FDecl);
     if (S && S->getFnParent())
     {
-      if (FunctionDecl* Parent = dyn_cast<FunctionDecl>(S->getFnParent()->getEntity()))
-        SemaRef.CheckCheerpFFICall(Parent, FDecl, Fn->getLocStart());
+      if (FunctionDecl* Parent = dyn_cast<FunctionDecl>(S->getFnParent()->getEntity())) {
+        SemaRef.CheckCheerpFFICall(Parent, FDecl, Fn->getLocStart(), Args);
+      }
     }
     return SemaRef.BuildResolvedCallExpr(Fn, FDecl, LParenLoc, Args, RParenLoc,
                                          ExecConfig, /*IsExecConfig=*/false,
@@ -12595,7 +12596,7 @@ Sema::CreateOverloadedBinOp(SourceLocation OpLoc,
         if (S && S->getFnParent())
         {
           if (FunctionDecl* Parent = dyn_cast<FunctionDecl>(S->getFnParent()->getEntity()))
-            CheckCheerpFFICall(Parent, FnDecl, OpLoc);
+            CheckCheerpFFICall(Parent, FnDecl, OpLoc, Args);
         }
         // Convert the arguments.
         if (CXXMethodDecl *Method = dyn_cast<CXXMethodDecl>(FnDecl)) {
@@ -12839,7 +12840,7 @@ Sema::CreateOverloadedArraySubscriptExpr(SourceLocation LLoc,
         if (S && S->getFnParent())
         {
           if (FunctionDecl* Parent = dyn_cast<FunctionDecl>(S->getFnParent()->getEntity()))
-            CheckCheerpFFICall(Parent, FnDecl, RLoc);
+            CheckCheerpFFICall(Parent, FnDecl, RLoc, Args);
         }
         CheckMemberOperatorAccess(LLoc, Args[0], Args[1], Best->FoundDecl);
 
