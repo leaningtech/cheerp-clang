cbcb9ea913f9cbb4613a686e4c1ba485d028a616
Alessandro Pignotti <a.pignotti@sssup.it>
2018-05-09 06:42:09 +0200

    Initial support for passing parameters and returning values from __asm__

--- lib/CodeGen/CGStmt.cpp
+++ lib/CodeGen/CGStmt.cpp
@@ -2006,7 +2006,16 @@ void CodeGenFunction::EmitAsmStmt(const AsmStmt &S) {
         LargestVectorWidth = std::max(LargestVectorWidth,
                                       VT->getPrimitiveSizeInBits());
     } else {
-      ArgTypes.push_back(Dest.getAddress().getType());
+      if (llvm::Type* AdjTy =
+            getTargetHooks().adjustInlineAsmType(*this, OutputConstraint,
+                                                 Dest.getAddress()->getType()))
+        ArgTypes.push_back(AdjTy);
+      else {
+        ArgTypes.push_back(Dest.getAddress().getType());
+        CGM.getDiags().Report(S.getAsmLoc(),
+                              diag::err_asm_invalid_type_in_input)
+            << OutExpr->getType() << OutputConstraint;
+      }
       Args.push_back(Dest.getPointer());
       Constraints += "=*";
       Constraints += OutputConstraint;
