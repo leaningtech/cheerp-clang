3392a315be6b120ec61c187f50962c5bbd4980e9
Alessandro Pignotti <a.pignotti@sssup.it>
2018-10-22 10:56:49 +0200

    Improve robustness of type safe malloc detection

--- lib/CodeGen/CGBuiltin.cpp
+++ lib/CodeGen/CGBuiltin.cpp
@@ -7220,7 +7220,7 @@ Value *CodeGenFunction::EmitCheerpBuiltinExpr(unsigned BuiltinID,
     const Stmt* parent=PM.getParent(E);
     // We need an explicit cast after the call, void* can't be used
     llvm::Type *Tys[] = { VoidPtrTy };
-    const CastExpr* retCE=dyn_cast<CastExpr>(parent);
+    const CastExpr* retCE=dyn_cast_or_null<CastExpr>(parent);
     if (!retCE || retCE->getType()->isVoidPointerType())
     {
         if (asmjs) return 0;
@@ -7241,7 +7241,7 @@ Value *CodeGenFunction::EmitCheerpBuiltinExpr(unsigned BuiltinID,
     const Stmt* parent=PM.getParent(E);
     // We need an explicit cast after the call, void* can't be used
     llvm::Type *Tys[] = { VoidPtrTy };
-    const CastExpr* retCE=dyn_cast<CastExpr>(parent);
+    const CastExpr* retCE=dyn_cast_or_null<CastExpr>(parent);
     if (!retCE || retCE->getType()->isVoidPointerType())
     {
         if (asmjs) return 0;
@@ -7281,7 +7281,7 @@ Value *CodeGenFunction::EmitCheerpBuiltinExpr(unsigned BuiltinID,
     ParentMap PM(FD->getBody());
     const Stmt* parent=PM.getParent(E);
     // We need an explicit cast after the call, void* can't be used
-    const CastExpr* retCE=dyn_cast<CastExpr>(parent);
+    const CastExpr* retCE=dyn_cast_or_null<CastExpr>(parent);
     if (!retCE || retCE->getType()->isVoidPointerType())
     {
         if (asmjs) return 0;
