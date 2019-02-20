12a6420035953952ba980dc99f804544caa158de
Alessandro Pignotti <a.pignotti@sssup.it>
2019-02-20 22:52:26 +0100

    StructType: Use new API with immutable asmjs and bytelayout flags

--- lib/CodeGen/CGVTables.cpp
+++ lib/CodeGen/CGVTables.cpp
@@ -1137,8 +1137,6 @@ llvm::Type* CodeGenTypes::GetVTableBaseType(bool asmjs)
   if(!ResultType)
   {
     llvm::StructType* ty = llvm::StructType::create(CGM.getLLVMContext(), typeName);
-    if (asmjs)
-      ty->setAsmJS();
     ResultType = ty;
   }
   return ResultType;
@@ -1176,7 +1174,7 @@ llvm::Type* CodeGenTypes::GetVTableSubObjectType(CodeGenModule& CGM,
     VTableTypes.push_back(OffsetTy);
   }
   llvm::StructType* ret = llvm::StructType::get(CGM.getLLVMContext(), VTableTypes,
-                            false, cast<llvm::StructType>(CGM.getTypes().GetVTableBaseType(asmjs)), asmjs);
+                            false, cast<llvm::StructType>(CGM.getTypes().GetVTableBaseType(asmjs)), /*isByteLayout*/false, asmjs);
   return ret;
 }
 
@@ -1219,7 +1217,7 @@ llvm::Type* CodeGenTypes::GetBasicVTableType(uint32_t virtualMethodsCount, bool 
   for(uint32_t j=0;j<virtualMethodsCount;j++)
     VTableTypes.push_back(FuncPtrTy);
 
-  return llvm::StructType::get(getLLVMContext(), VTableTypes, false, cast<llvm::StructType>(GetVTableBaseType(asmjs)), asmjs);
+  return llvm::StructType::get(getLLVMContext(), VTableTypes, false, cast<llvm::StructType>(GetVTableBaseType(asmjs)), /*isByteLayout*/false, asmjs);
 }
 
 llvm::Type* CodeGenTypes::GetClassTypeInfoType()
