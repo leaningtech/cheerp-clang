adc8120bdf7648c52799ac11eee7a2402c33e5ad
Alessandro Pignotti <a.pignotti@sssup.it>
2019-02-24 13:35:44 +0000

    Fixes for new driver API

--- lib/Driver/ToolChains/WebAssembly.cpp
+++ lib/Driver/ToolChains/WebAssembly.cpp
@@ -316,7 +316,7 @@ void cheerp::Link::ConstructJob(Compilation &C, const JobAction &JA,
   }
 
   const char *Exec = Args.MakeArgString((getToolChain().GetProgramPath("llvm-link")));
-  C.addCommand(llvm::make_unique<Command>(JA, *this, Exec, CmdArgs));
+  C.addCommand(llvm::make_unique<Command>(JA, *this, Exec, CmdArgs, Inputs));
 }
 
 void cheerp::CheerpOptimizer::ConstructJob(Compilation &C, const JobAction &JA,
@@ -354,7 +354,7 @@ void cheerp::CheerpOptimizer::ConstructJob(Compilation &C, const JobAction &JA,
     CheerpNoPointerSCEV->render(Args, CmdArgs);
 
   const char *Exec = Args.MakeArgString((getToolChain().GetProgramPath("opt")));
-  C.addCommand(llvm::make_unique<Command>(JA, *this, Exec, CmdArgs));
+  C.addCommand(llvm::make_unique<Command>(JA, *this, Exec, CmdArgs, Inputs));
 }
 
 void cheerp::CheerpCompiler::ConstructJob(Compilation &C, const JobAction &JA,
@@ -445,5 +445,5 @@ void cheerp::CheerpCompiler::ConstructJob(Compilation &C, const JobAction &JA,
   Args.AddAllArgValues(CmdArgs, options::OPT_mllvm);
 
   const char *Exec = Args.MakeArgString((getToolChain().GetProgramPath("llc")));
-  C.addCommand(llvm::make_unique<Command>(JA, *this, Exec, CmdArgs));
+  C.addCommand(llvm::make_unique<Command>(JA, *this, Exec, CmdArgs, Inputs));
 }
