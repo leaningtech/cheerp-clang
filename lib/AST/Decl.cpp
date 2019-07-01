c10452327d557b67885b11fa869c1d9773927161
Alessandro Pignotti <a.pignotti@sssup.it>
2019-07-01 11:48:26 +0200

    Do not add the bytelayout tag to asmjs unions

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -4307,6 +4307,8 @@ void RecordDecl::setCapturedRecord() {
 
 bool RecordDecl::isByteLayout() const
 {
+  if (hasAttr<AsmJSAttr>())
+    return false;
   // Unions and anonymous structures inside unions use bytelayout
   const RecordDecl *CurDecl = this;
   while (CurDecl && (CurDecl->isUnion() || CurDecl->isAnonymousStructOrUnion()))
