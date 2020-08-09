91a959bc5a061c6495ab9b05cae5950170f15ccc
Alessandro Pignotti <a.pignotti@sssup.it>
2020-08-09 15:07:14 +0000

    Remove use of deprecated API

--- lib/Sema/SemaExprCXX.cpp
+++ lib/Sema/SemaExprCXX.cpp
@@ -1723,7 +1723,7 @@ Sema::ActOnCXXNew(SourceLocation StartLoc, bool UseGlobal,
     }
   }
 
-  return BuildCXXNew(SourceRange(StartLoc, D.getLocEnd()), UseGlobal,
+  return BuildCXXNew(SourceRange(StartLoc, D.getEndLoc()), UseGlobal,
                      PlacementLParen,
                      PlacementArgs,
                      PlacementRParen,
